12bac87bb0440de6698489dd6438e9984559177b
Ryan Day <ryandaydev@gmail.com>
2025-09-17 13:20:35 +0000

    Remove extra files

--- football/football_client.py
+++ football/football_client.py
@@ -1,11 +1,11 @@
 import asyncio
 from fastmcp import Client
 
-client = Client("football_server.py")
+client = Client("https://ryan-football-server.fastmcp.app/mcp")
 
-async def call_tool(name: str):
+async def call_tool():
     async with client:
-        result = await client.call_tool("greet", {"name": name})
+        result = await client.call_tool("get_counts")
         print(result)
 
-asyncio.run(call_tool("Chevy"))
+asyncio.run(call_tool())
